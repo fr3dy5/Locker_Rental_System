d3dba5a7d9943f6904ccb0e3c358ab63128bc533
fr3dy5 <fr3dy5al@gmail.com>
2024-10-02 19:43:21 -0500

    first commit, trying more consistent pushing

---
+++ locker_rental_system.c
@@ -0,0 +1,31 @@
+/*
+Create a program designed to simulate the management of locker rentals that 
+1. allows users to rent lockers, 
+2. view the contents of a specific locker, 
+3. end a locker rental, 
+4. list the contents of all rented lockers.
+Program should provide a menu-driven interface for users to make choices 
+and can be implemented using 
+1. constants, 
+2. arrays, 
+3. strings, 
+4. functions, 
+5. standard library functions, 
+6.switch and break statements, 
+7. loops, 
+8.Boolean operators 
+9. conditional statements.
+*/
+
+/*
+switch cases:
+Locker Rental Menu
+=============================
+1. View a locker
+2. Rent a locker
+3. End a locker rental
+4. List all locker contents
+5. Quit
+
+Enter your choice (1-5): 
+*/
